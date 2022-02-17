f92d11c0800325ed78d1360fd940c2092a440521
Ramiro Serra <serraramiro@ekumenlabs.com>
2022-02-17 19:19:13 -0300

    initial setup

---
+++ run.sh
@@ -0,0 +1,12 @@
+RUN_CMD="cd /app && g++ main.cpp && ./a.out"
+CONTAINER_NAME="mooveit_challenge"
+if [ ! $(docker ps -q -f name="^${CONTAINER_NAME}$") ]; then
+docker build docker -t $CONTAINER_NAME
+docker run -dit --rm \
+  --name $CONTAINER_NAME \
+  -v $PWD/src:/app \
+  $CONTAINER_NAME bash
+fi
+
+docker exec -it $CONTAINER_NAME bash -c "$RUN_CMD"
+
